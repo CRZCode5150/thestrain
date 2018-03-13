960e1590d32b3bb20aa4c584b0757960dad2e8ae
Unknown <chrisramirez15@yahoo.com>
2018-03-13 13:07:21 -0500

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,56 @@
+@extends('master')
+
+
+@section('content')
+
+    <div class="row">
+        <div class=col-4>
+            <div class="circle">Girl Scout Cookies</div>
+        </div>
+
+        <div id="paddingrop1" class="col-4">
+                <ul id="moveul">
+                    <li>Race:</li>
+                    <br>
+                    <li>Flavor:</li>
+                    <br>
+                    <li>Effect:</li4>
+                </ul>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class=col-4>
+            <div class="circle">Girl Scout Cookies</div>
+        </div>
+        <div id="paddingrop1" class="col-4">
+                <ul id="moveul">
+                    <li>Race:</li>
+                    <br>
+                    <li>Flavor:</li>
+                    <br>
+                    <li>Effect:</li4>
+                </ul>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class=col-4>
+            <div class="circle">Girl Scout Cookies</div>
+        </div>
+
+        <div id="paddingrop1" class="col-4">
+                <ul id="moveul">
+                    <li>Race:</li>
+                    <br>
+                    <li>Flavor:</li>
+                    <br>
+                    <li>Effect:</li4>
+                </ul>
+        </div>
+    </div>
+
+
+
+
+@endsection
