30b1d3265c337352be6a4b3aa006d90f4ac655e0
Unknown <chrisramirez15@yahoo.com>
2018-07-02 09:46:19 -0500

    :)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,13 @@
 @extends('master') @section('content')
 
-<div class ="row figure justify content-center">
-    <div class ="movehigher col-6">
+<div class="row figure justify-content-center">
+    <div class="movehigher search-form text-center col-6">
         <h2 class="heading">Search your strain</h2>
         <form action=/"get_strains" method="post">
             @crsf
+            <div class="control-group">
+            <input name="srain" class="form-control mb-2" type="search" placeholder="Search Strain" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+            <button class="btn btn-outline" type="submit" >Search</button>
     </div>
 </div>
 
